0a0dafbbf338f5cdea483f160de03452e6d8b707
Yerra Rahul <yerrarahul23@gmail.com>
2026-05-18 00:27:05 +0530

    feat: add experimental sticky card stack landing page and exclude test files from version control

---
+++ test-mode.js
@@ -0,0 +1,25 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const cards = gsap.utils.toArray(".card");
+
+cards.forEach((card, index) => {
+
+    if (index !== cards.length - 1) {
+
+        gsap.to(card, {
+            scale: 0.9,
+            rotation: -5,
+            opacity: 0,
+
+            scrollTrigger: {
+                trigger: card,
+                start: "top top",
+                end: "+=500",
+                scrub: true,
+                pin: true
+            }
+        });
+
+    }
+
+});
